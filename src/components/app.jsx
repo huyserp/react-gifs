0c5742c78ec99c76bdc9f28833e9825014ccd152
peter huyser <huyserp@gmail.com>
2021-04-11 14:49:39 +0200

    create components for app, gif, gifList, and searchBar

---
+++ src/components/app.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      gifs: [
+        { id: "wW95fEq09hOI8" },
+        { id: "wW95fEq09hOI8" }
+      ],
+      selectedGifId: "wW95fEq09hOI8"
+    }
+  }
+
+  search = (query) => {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId}/>
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default App;
